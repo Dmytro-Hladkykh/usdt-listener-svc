29ceb6e8f5f7918a5f9e5d874fdff0a97a2629cf
Dmytro-Hladkykh <152580956+Dmytro-Hladkykh@users.noreply.github.com>
2024-07-26 18:55:41 +0300

    Created usdt.go abigen Modified listener with batches and block inserting

--- internal/service/listener.go
+++ internal/service/listener.go
@@ -3,6 +3,7 @@ package service
 import (
 	"context"
 	"math/big"
+	"os"
 	"time"
 
 	"github.com/Dmytro-Hladkykh/usdt-listener-svc/internal/data"
@@ -16,7 +17,8 @@ import (
 
 const (
     USDTContractAddress = "0xdAC17F958D2ee523a2206206994597C13D831ec7"
-    ReconnectDelay      = 5 * time.Second
+    ReconnectDelay      = 15 * time.Second
+    MaxRetryAttempts    = 5
 )
 
 type Listener struct {
@@ -38,15 +40,154 @@ func NewListener(infuraURL string, db data.MasterQ, log *logan.Entry) (*Listener
 }
 
 func (l *Listener) Listen(ctx context.Context) error {
-    for {
-        if err := l.processHistoricalEvents(ctx); err != nil {
+    processHist := os.Getenv("PROCESS_HIST")
+
+    var lastProcessedBlock uint64
+    var err error
+
+    if processHist == "true" {
+        lastProcessedBlock, err = l.processHistoricalEvents(ctx)
+        if err != nil {
             l.log.WithError(err).Error("Failed to process historical events")
+        }
+    } else {
+        l.log.Info("Skipping historical events processing")
+        currentBlock, err := l.client.BlockNumber(ctx)
+        if err != nil {
+            return errors.Wrap(err, "failed to get current block number")
+        }
+        lastProcessedBlock = currentBlock
+        if err := l.db.LastProcessedBlock().Update(currentBlock); err != nil {
+            l.log.WithError(err).Error("Failed to update last processed block")
+        }
+    }
+
+    return l.listenForNewEvents(ctx, lastProcessedBlock)
+}
+
+func (l *Listener) processHistoricalEvents(ctx context.Context) (uint64, error) {
+    lastProcessedBlock, err := l.db.LastProcessedBlock().Get()
+    if err != nil {
+        return 0, errors.Wrap(err, "failed to get last processed block")
+    }
+
+    for {
+        currentBlock, err := l.client.BlockNumber(ctx)
+        if err != nil {
+            return lastProcessedBlock, errors.Wrap(err, "failed to get current block number")
+        }
+
+        if lastProcessedBlock >= currentBlock {
+            break
+        }
+
+        l.log.WithFields(logan.F{
+            "lastProcessedBlock": lastProcessedBlock,
+            "currentBlock":       currentBlock,
+        }).Info("Processing historical blocks")
+
+        batchSize := uint64(10)
+        for blockNum := lastProcessedBlock + 1; blockNum <= currentBlock; blockNum += batchSize {
+            endBlock := blockNum + batchSize - 1
+            if endBlock > currentBlock {
+                endBlock = currentBlock
+            }
+
+            l.log.WithFields(logan.F{
+                "fromBlock": blockNum,
+                "toBlock":   endBlock,
+            }).Info("Processing block range")
+
+            if err := l.processBlockRange(ctx, blockNum, endBlock); err != nil {
+                l.log.WithError(err).Error("Failed to process block range")
+                time.Sleep(ReconnectDelay)
+                continue
+            }
+
+            lastProcessedBlock = endBlock
+            if err := l.db.LastProcessedBlock().Update(lastProcessedBlock); err != nil {
+                l.log.WithError(err).Error("Failed to update last processed block")
+            }
+        }
+    }
+
+    return lastProcessedBlock, nil
+}
+
+func (l *Listener) processBlockRange(ctx context.Context, startBlock, endBlock uint64) error {
+    for blockNum := startBlock; blockNum <= endBlock; blockNum++ {
+        if err := l.processBlock(ctx, blockNum); err != nil {
+            l.log.WithError(err).WithField("blockNumber", blockNum).Error("Failed to process block")
+            continue
+        }
+
+        if err := l.db.Transaction(func(q data.MasterQ) error {
+            if err := q.LastProcessedBlock().Update(blockNum); err != nil {
+                return errors.Wrap(err, "failed to update last processed block")
+            }
+            return nil
+        }); err != nil {
+            l.log.WithError(err).WithField("blockNumber", blockNum).Error("Failed to update last processed block")
+        }
+    }
+    return nil
+}
+
+func (l *Listener) processBlock(ctx context.Context, blockNum uint64) error {
+    contractAddress := common.HexToAddress(USDTContractAddress)
+    query := ethereum.FilterQuery{
+        FromBlock: big.NewInt(int64(blockNum)),
+        ToBlock:   big.NewInt(int64(blockNum)),
+        Addresses: []common.Address{contractAddress},
+    }
+
+    logs, err := l.client.FilterLogs(ctx, query)
+    if err != nil {
+        return errors.Wrap(err, "failed to filter logs")
+    }
+
+    transactions := make([]data.USDTTransfer, 0, len(logs))
+    for _, vLog := range logs {
+        tx, err := l.extractTransaction(vLog)
+        if err != nil {
+            l.log.WithFields(logan.F{
+                "blockNumber": vLog.BlockNumber,
+                "logIndex":    vLog.Index,
+            }).WithError(err).Error("Error processing log")
+            continue
+        }
+        transactions = append(transactions, tx)
+    }
+
+    if len(transactions) > 0 {
+        if err := l.db.USDTTransfer().InsertBlock(transactions); err != nil {
+            return errors.Wrap(err, "failed to insert block of USDT transfers")
+        }
+    }
+
+    return nil
+}
+
+func (l *Listener) listenForNewEvents(ctx context.Context, startBlock uint64) error {
+    for {
+        currentBlock, err := l.client.BlockNumber(ctx)
+        if err != nil {
+            l.log.WithError(err).Error("Failed to get current block number")
             time.Sleep(ReconnectDelay)
             continue
         }
 
-        if err := l.listenForNewEvents(ctx); err != nil {
-            l.log.WithError(err).Error("Error listening for new events")
+        if startBlock < currentBlock {
+            if err := l.processBlockRange(ctx, startBlock+1, currentBlock); err != nil {
+                l.log.WithError(err).Error("Failed to process missed blocks")
+                time.Sleep(ReconnectDelay)
+                continue
+            }
+            startBlock = currentBlock
+        }
+
+        if err := l.subscribeToNewLogs(ctx, startBlock+1); err != nil {
+            l.log.WithError(err).Error("Error in log subscription")
             time.Sleep(ReconnectDelay)
             continue
         }
@@ -60,85 +201,10 @@ func (l *Listener) Listen(ctx context.Context) error {
     }
 }
 
-func (l *Listener) processHistoricalEvents(ctx context.Context) error {
-    lastProcessedBlock, err := l.db.LastProcessedBlock().Get()
-    if err != nil {
-        return errors.Wrap(err, "failed to get last processed block")
-    }
-
-    currentBlock, err := l.client.BlockNumber(ctx)
-    if err != nil {
-        return errors.Wrap(err, "failed to get current block number")
-    }
-
-    if lastProcessedBlock < currentBlock {
-        l.log.WithFields(logan.F{
-            "lastProcessedBlock": lastProcessedBlock,
-            "currentBlock":       currentBlock,
-        }).Info("Processing missed blocks")
-
-        contractAddress := common.HexToAddress(USDTContractAddress)
-
-        for blockNum := lastProcessedBlock + 1; blockNum <= currentBlock; blockNum++ {
-            l.log.WithFields(logan.F{
-                "blockNumber": blockNum,
-            }).Info("Processing block")
-
-            query := ethereum.FilterQuery{
-                FromBlock: big.NewInt(int64(blockNum)),
-                ToBlock:   big.NewInt(int64(blockNum)),
-                Addresses: []common.Address{contractAddress},
-            }
-
-            logs, err := l.client.FilterLogs(ctx, query)
-            if err != nil {
-                l.log.WithFields(logan.F{
-                    "blockNumber": blockNum,
-                }).WithError(err).Error("Failed to filter logs")
-                continue
-            }
-
-            transactions := make([]data.USDTTransfer, 0)
-            for _, vLog := range logs {
-                tx, err := l.extractTransaction(vLog)
-                if err != nil {
-                    l.log.WithFields(logan.F{
-                        "blockNumber": blockNum,
-                        "logIndex":    vLog.Index,
-                    }).WithError(err).Error("Error processing historical log")
-                    continue
-                }
-                transactions = append(transactions, tx)
-            }
-
-            if len(transactions) > 0 {
-                if err := l.db.USDTTransfer().InsertBlock(transactions); err != nil {
-                    l.log.WithFields(logan.F{
-                        "blockNumber": blockNum,
-                    }).WithError(err).Error("Failed to insert block of USDT transfers")
-                    continue
-                }
-            }
-
-            if err := l.db.LastProcessedBlock().Update(blockNum); err != nil {
-                l.log.WithFields(logan.F{
-                    "blockNumber": blockNum,
-                }).WithError(err).Error("Failed to update last processed block")
-                continue
-            }
-
-            l.log.WithFields(logan.F{
-                "blockNumber": blockNum,
-            }).Info("Successfully processed block")
-        }
-    }
-
-    return nil
-}
-
-func (l *Listener) listenForNewEvents(ctx context.Context) error {
+func (l *Listener) subscribeToNewLogs(ctx context.Context, fromBlock uint64) error {
     contractAddress := common.HexToAddress(USDTContractAddress)
     query := ethereum.FilterQuery{
+        FromBlock: big.NewInt(int64(fromBlock)),
         Addresses: []common.Address{contractAddress},
     }
     logs := make(chan types.Log)
@@ -146,7 +212,10 @@ func (l *Listener) listenForNewEvents(ctx context.Context) error {
     if err != nil {
         return errors.Wrap(err, "failed to subscribe to logs")
     }
-    l.log.Info("Started listening for new USDT transfers")
+    defer sub.Unsubscribe()
+
+    l.log.WithField("fromBlock", fromBlock).Info("Started listening for new USDT transfers")
+
     for {
         select {
         case err := <-sub.Err():
